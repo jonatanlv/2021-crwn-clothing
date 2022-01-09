b29b1020a12a0f33d8c8fb500c6e62c43626752f
jonatan.lara <jonatan.lara@gft.com>
2022-01-09 23:39:00 +0100

    Navegación básica

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import "./App.css";
+import { Route, Switch } from "react-router-dom";
 
 import HomePage from "./pages/homepage/homepage.component";
 
+const HatsPage = () => (
+  <div>
+    <h1>THE HATS PAGE</h1>
+  </div>
+);
+
 function App() {
-  return <HomePage />;
+  return (
+    <div>
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/hats" component={HatsPage} />
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
