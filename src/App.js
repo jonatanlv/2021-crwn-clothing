c2f989a4092386b5543e0dc3c674632190d759d2
jonatan.lara <jonatan.lara@gft.com>
2022-01-23 21:35:29 +0100

    Primeros pasos en el formulario de registro

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Route, Switch } from "react-router-dom";
 
 import Header from "./components/header/header.component";
 import HomePage from "./pages/homepage/homepage.component";
+import SignInAndSignUpPage from "./pages/signin-and-signup/signin-and-signup.component";
 import ShopPage from "./pages/shop/shop.component";
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
+        <Route path="/signin" component={SignInAndSignUpPage} />
       </Switch>
     </div>
   );
---
+++ src/pages/signin-and-signup/signin-and-signup.component.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+import "./signin-and-signup.styles.scss";
+
+import SignIn from "../../components/sign-in/sign-in.component";
+
+const SignInAndSignUpPage = () => <SignIn />;
+
+export default SignInAndSignUpPage;
