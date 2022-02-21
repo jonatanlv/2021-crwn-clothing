3496f85c6024f402a0a1404b98137d4f8d0ab606
jonatan.lara <jonatan.lara@gft.com>
2022-02-21 20:11:49 +0100

    Mover directory y shop a su propio reducer

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -1,21 +1,21 @@
 import React from "react";
 import { connect } from "react-redux";
 
-import { createStructuredSelector } from "reselect";
-
 import CollectionItem from "../../components/collection-item/collection-item.component";
-import {
-  selectShopCollections,
-  selectCollection,
-} from "../../redux/shop/shop.selectors";
+import { selectCollection } from "../../redux/shop/shop.selectors";
 
 import "./collection.styles.scss";
 
 const CollectionPage = ({ match, collection }) => {
-  console.log(collection);
+  const { title, items } = collection;
   return (
     <div className="collection-page">
-      <h2>Collection page {match.params.collectionId}</h2>
+      <h2 className="title">{title}</h2>
+      <div className="items">
+        {items.map((item) => (
+          <CollectionItem key={item.id} item={item} />
+        ))}
+      </div>
     </div>
   );
 };
